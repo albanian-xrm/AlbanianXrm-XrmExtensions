22b66e35d31862230ded1fe7b713d3110bf706a8
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2021-12-18 19:54:11 +0100

    :rocket: First release

---
+++ NuGetPack.ps1
@@ -0,0 +1,29 @@
+﻿ param(   
+    [switch]$forceDownload,
+    [string]$packageVersion = $null
+ )
+
+if($packageVersion -eq ""){
+    $releases = & git tag -l v*
+    if($releases -eq $null -or ($releases -eq '')){
+        $packageVersion = "0.1.0"
+    } 
+    else {
+        $packageVersion = ($releases | Sort-Object -Descending | Select -First 1).TrimStart('v')
+    }
+}
+
+if(!(Test-Path -Path ".\packages")){
+    New-Item -Name ".\packages" -ItemType Directory
+}
+
+if(!(Test-Path -Path ".\packages\NuGet.exe" -PathType Leaf) -or $forceDownload){
+    Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile ".\packages\NuGet.exe"
+}
+
+& ".\packages\NuGet.exe" pack ".\AlbanianXrm.XrmExtensions.Source\AlbanianXrm.XrmExtensions.8.nuspec" -OutputDirectory ".\packages" -Version $packageVersion
+& ".\packages\NuGet.exe" pack ".\AlbanianXrm.XrmExtensions.Source\AlbanianXrm.XrmExtensions.9.nuspec" -OutputDirectory ".\packages" -Version $packageVersion
+& ".\packages\NuGet.exe" pack ".\AlbanianXrm.XrmPluginExtensions.Source\AlbanianXrm.XrmPluginExtensions.8.nuspec" -OutputDirectory ".\packages" -Version $packageVersion
+& ".\packages\NuGet.exe" pack ".\AlbanianXrm.XrmPluginExtensions.Source\AlbanianXrm.XrmPluginExtensions.9.nuspec" -OutputDirectory ".\packages" -Version $packageVersion
+& ".\packages\NuGet.exe" pack ".\AlbanianXrm.XrmWorkflowExtensions.Source\AlbanianXrm.XrmWorkflowExtensions.8.nuspec" -OutputDirectory ".\packages" -Version $packageVersion
+& ".\packages\NuGet.exe" pack ".\AlbanianXrm.XrmWorkflowExtensions.Source\AlbanianXrm.XrmWorkflowExtensions.9.nuspec" -OutputDirectory ".\packages" -Version $packageVersion
---
+++ NuGetPush.ps1
@@ -0,0 +1,14 @@
+﻿ param(   
+    [string]$ApiKey
+ )
+
+
+if(!(Test-Path -Path ".\packages\NuGet.exe" -PathType Leaf)){
+    Write-Error "NuGet.exe is missing" -ErrorAction Stop 
+}
+
+$packages = Get-Item .\packages\*.nupkg
+
+foreach($package in $packages){
+  & .\packages\NuGet.exe push "$package" -ApiKey "$ApiKey" -Source https://api.nuget.org/v3/index.json 
+}
